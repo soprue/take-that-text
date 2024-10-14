efb109fbef196f903bae0f0742bef1e4452b98e2
Gahee Kim <soprue@naver.com>
2024-10-14 21:46:28 +0900

    chore: tailwind 설치 및 설정

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <q-page class="row items-center justify-evenly"> hello </q-page>
+  <q-page class="row items-center justify-evenly bg-[#c2c2c2] text-white">
+    hello
+  </q-page>
 </template>
 
 <script setup lang="ts">
