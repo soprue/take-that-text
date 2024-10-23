7dfda3448f6326f6f30ec224379746ae8ebc9b62
Gahee Kim <soprue@naver.com>
2024-10-23 22:26:06 +0900

    feat: 인풋 유효성 검사 추가 및 스타일 수정

--- src/components/ChatInputBox.vue
+++ src/components/ChatInputBox.vue
@@ -4,28 +4,45 @@
       <textarea
         placeholder="텍스트를 입력해 주세요!"
         class="max-h-[150px] w-full resize-none text-[#333] outline-none"
-        rows="{1}"
+        rows="1"
         ref="textarea"
-        @input="handleAutoResize"
-        @change="handleChangeInput"
+        @input="handleInput"
       ></textarea>
     </div>
-    <div
-      class="flex w-1/12 cursor-pointer items-center justify-center bg-[#f7f7f7]"
+    <button
+      class="flex w-1/12 items-center justify-center bg-[#f7f7f7]"
+      :class="{
+        '!cursor-default': isEmptyContents,
+        'cursor-pointer': !isEmptyContents,
+        '!opacity-100': true,
+      }"
+      :disabled="isEmptyContents ? true : false"
     >
-      <q-icon name="fa-solid fa-reply" size="20px" style="color: #ccc" />
-    </div>
+      <q-icon
+        name="fa-solid fa-reply"
+        size="20px"
+        :style="{
+          color: isEmptyContents ? '#ccc' : '#555',
+          cursor: isEmptyContents ? 'default !important' : 'pointer',
+        }"
+      />
+    </button>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 
 const textarea = ref(null);
 let contents = ref('');
 
-const handleChangeInput = (e) => {
-  return (contents.value = e.target.value);
+const isEmptyContents = computed(() => {
+  return contents.value.trim().length === 0;
+});
+
+const handleInput = (e) => {
+  contents.value = e.target.value;
+  handleAutoResize();
 };
 
 const handleAutoResize = () => {
