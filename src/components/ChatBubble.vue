21d7fe1576403c0a3fd1d8695a7843d4aa2e7a75
Gahee Kim <soprue@naver.com>
2024-10-23 23:09:14 +0900

    feat: 말풍선 레이아웃 구현

---
+++ src/components/ChatBubble.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="w-fit max-w-[450px] whitespace-pre-line rounded-xl bg-white px-5 py-4 text-[#1b000f] shadow-sm"
+    :class="align === 'left' ? 'self-start' : 'self-end'"
+  >
+    {{ text }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  text: String,
+  align: String,
+});
+</script>
--- src/components/ChatContainer.vue
+++ src/components/ChatContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex h-[777px] max-h-[80%] w-[730px] max-w-[80%] flex-col rounded-3xl bg-[#eee] p-2"
+    class="flex h-[777px] max-h-[80%] w-[730px] max-w-[80%] flex-col rounded-3xl bg-[#eee] p-4"
   >
     <div class="flex items-center gap-3 p-6">
       <q-icon
@@ -14,7 +14,11 @@
     </div>
 
     <div class="flex w-full flex-1 flex-col justify-end">
-      <div></div>
+      <div class="mb-4 flex flex-col items-stretch gap-3">
+        <ChatBubble :text="text1" align="left" />
+        <ChatBubble :text="text2" align="right" />
+        <ChatBubble :text="text3" align="left" />
+      </div>
       <ChatInputBox />
     </div>
   </div>
@@ -22,4 +26,17 @@
 
 <script setup>
 import ChatInputBox from '@components/ChatInputBox.vue';
+import ChatBubble from '@components/ChatBubble.vue';
+
+let text1 = '꾸미고 싶은 텍스트를 입력해 보세요! 🌟 예쁘게 만들어 드릴게요. 😉';
+let text2 = `얄미워 너 정말
+왜 저럴까 진짜?
+내가 알던 핑크빛 Romance는
+둥근 풍선 같던데
+우린 좌 우 앞 뒤 뾰족하다구!`;
+let text3 = `얄미워😤 너🫵 정말😠
+왜 저럴까🤔 진짜⁉️
+내가 알던 핑크빛🎀 Romance💖는
+둥근 풍선🎈 같던데💭
+우린 좌↔️ 우↔️ 앞⬆️ 뒤⬇️ 뾰족하다구!⚔️🌀`;
 </script>
