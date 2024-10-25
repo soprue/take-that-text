678b0a4bf25ccd336c632747ecfc47119c903bba
soprue <soprue@naver.com>
2024-10-25 11:38:21 +0900

    feat: 말풍선 올라가는 애니메이션 추가

--- src/components/ChatBubble.vue
+++ src/components/ChatBubble.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-fit max-w-[450px] whitespace-pre-line rounded-xl bg-white px-5 py-4 text-[#1b0100] shadow-sm"
+    class="bubble w-fit max-w-[450px] whitespace-pre-line rounded-xl bg-white px-5 py-4 text-[#1b0100] shadow-sm"
     :class="align === 'left' ? 'self-start' : 'self-end'"
   >
     {{ text }}
@@ -13,3 +13,20 @@ defineProps({
   align: String,
 });
 </script>
+
+<style scoped>
+@keyframes slideUp {
+  from {
+    transform: translateY(30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+.bubble {
+  animation: slideUp 0.7s ease-out;
+}
+</style>
--- src/components/ChatContainer.vue
+++ src/components/ChatContainer.vue
@@ -14,22 +14,54 @@
     </div>
 
     <div class="flex w-full flex-1 flex-col justify-end">
-      <div class="mb-4 flex flex-col items-stretch gap-3">
-        <ChatBubble v-for="(text, index) in texts" :key="index" :text="text.text" :align="text.align" />
-
-      </div>
+      <!-- transition-group을 적용하여 말풍선들이 부드럽게 등장하고 밀려나도록 설정 -->
+      <transition-group name="chat" tag="div" class="mb-4 flex flex-col items-stretch gap-3">
+        <ChatBubble v-for="(text, index) in displayedTexts" :key="index" :text="text.text" :align="text.align" />
+      </transition-group>
       <ChatInputBox />
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+
 import ChatInputBox from '@components/ChatInputBox.vue';
 import ChatBubble from '@components/ChatBubble.vue';
 
 const texts = [
-  { text: '꾸미고 싶은 텍스트를 입력해 보세요! 🌟 예쁘게 만들어 드릴게요. 😉', align: 'left' },
+  { text: '꾸미고 싶은 텍스트를 입력해 보세요! 🌟 아래처럼 예쁘게 만들어 드릴게요. 😉', align: 'left' },
   { text: '얄미워 너 정말\n왜 저럴까 진짜?\n내가 알던 핑크빛 Romance는\n둥근 풍선 같던데\n우린 좌 우 앞 뒤 뾰족하다구!', align: 'right' },
   { text: '얄미워😤 너🫵 정말😠\n왜 저럴까🤔 진짜⁉️\n내가 알던 핑크빛🎀 Romance💖는\n둥근 풍선🎈 같던데💭\n우린 좌↔️ 우↔️ 앞⬆️ 뒤⬇️ 뾰족하다구!⚔️🌀', align: 'left' }
 ];
+
+const displayedTexts = ref([]);
+
+onMounted(() => {
+  texts.forEach((text, index) => {
+    setTimeout(() => {
+      displayedTexts.value.push(text);
+    }, index * 1000); 
+  });
+});
 </script>
+
+<style scoped>
+.chat-enter-active {
+  transition: all 0.5s ease;
+}
+
+.chat-enter-from {
+  transform: translateY(20px);
+  opacity: 0;
+}
+
+.chat-enter-to {
+  transform: translateY(0);
+  opacity: 1;
+}
+
+.chat-move {
+  transition: transform 0.5s ease-out;
+}
+</style>
