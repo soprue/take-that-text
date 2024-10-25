5f6c0bd0ab5c2e5cc4fc95b3c0d574cfa118c4e4
soprue <soprue@naver.com>
2024-10-25 11:52:56 +0900

    feat: 새로운 말풍선이 추가될 때 스크롤이 자동으로 맨아래로 내려지는 기능 구현

--- src/components/ChatContainer.vue
+++ src/components/ChatContainer.vue
@@ -2,29 +2,30 @@
   <div
     class="flex h-[777px] max-h-[80%] w-[730px] max-w-[80%] flex-col rounded-3xl bg-[#eee] p-4"
   >
-    <div class="flex items-center gap-3 p-6">
+    <div class="flex items-center gap-3 p-6 h-[77px]">
       <q-icon
         name="fa-solid fa-wand-magic-sparkles"
         size="20px"
         style="color: #b7136b"
       />
-      <p class="text- select-none text-xl font-bold text-[#1b0100]">
+      <p class="text-select-none text-xl font-bold text-[#1b0100]">
         Take That Text
       </p>
     </div>
 
-    <div class="flex w-full flex-1 flex-col justify-end">
-      <!-- transition-group을 적용하여 말풍선들이 부드럽게 등장하고 밀려나도록 설정 -->
-      <transition-group name="chat" tag="div" class="mb-4 flex flex-col items-stretch gap-3">
-        <ChatBubble v-for="(text, index) in displayedTexts" :key="index" :text="text.text" :align="text.align" />
-      </transition-group>
+    <div class="flex w-full flex-1 flex-col justify-end h-[700px]">
+      <div ref="chatContainer" class="flex-1 overflow-y-auto" style="max-height: 600px;">
+        <transition-group name="chat" tag="div" class="mb-4 flex flex-col items-stretch gap-3">
+          <ChatBubble v-for="(text, index) in displayedTexts" :key="index" :text="text.text" :align="text.align" />
+        </transition-group>
+      </div>
       <ChatInputBox />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, nextTick } from 'vue';
 
 import ChatInputBox from '@components/ChatInputBox.vue';
 import ChatBubble from '@components/ChatBubble.vue';
@@ -32,18 +33,29 @@ import ChatBubble from '@components/ChatBubble.vue';
 const texts = [
   { text: '꾸미고 싶은 텍스트를 입력해 보세요! 🌟 아래처럼 예쁘게 만들어 드릴게요. 😉', align: 'left' },
   { text: '얄미워 너 정말\n왜 저럴까 진짜?\n내가 알던 핑크빛 Romance는\n둥근 풍선 같던데\n우린 좌 우 앞 뒤 뾰족하다구!', align: 'right' },
-  { text: '얄미워😤 너🫵 정말😠\n왜 저럴까🤔 진짜⁉️\n내가 알던 핑크빛🎀 Romance💖는\n둥근 풍선🎈 같던데💭\n우린 좌↔️ 우↔️ 앞⬆️ 뒤⬇️ 뾰족하다구!⚔️🌀', align: 'left' }
+  { text: '얄미워😤 너🫵 정말😠\n왜 저럴까🤔 진짜⁉️\n내가 알던 핑크빛🎀 Romance💖는\n둥근 풍선🎈 같던데💭\n우린 좌↔️ 우↔️ 앞⬆️ 뒤⬇️ 뾰족하다구!⚔️🌀', align: 'left' },
 ];
 
 const displayedTexts = ref([]);
+const chatContainer = ref(null);
 
 onMounted(() => {
   texts.forEach((text, index) => {
     setTimeout(() => {
       displayedTexts.value.push(text);
+      scrollToBottom(); 
     }, index * 1000); 
   });
 });
+
+const scrollToBottom = () => {
+  nextTick(() => {
+    const container = chatContainer.value;
+    if (container) {
+      container.scrollTop = container.scrollHeight;
+    }
+  });
+};
 </script>
 
 <style scoped>
