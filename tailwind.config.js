333663e31ca16c512c0968a5b61a42e77de21ae9
soprue <soprue@naver.com>
2024-11-08 21:41:20 +0900

    chore: 반응형 디자인을 위한 config 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
     extend: {
+      screens: {
+        'max-xs': { max: '480px' },
+      },
       fontFamily: {
         sans: ['Pretendard', 'sans-serif'],
       },
